b7bb7bddead5365eda30c54badc6ab82261a8b5a
ABHISHEK KUMAR <96915839+abhishek-kuma@users.noreply.github.com>
2023-02-05 19:15:14 +0530

    Add files via upload

--- leetcode_83.cpp
+++ leetcode_83.cpp
@@ -1,52 +1,29 @@
-#include <iostream>
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-struct ListNode
-{
+struct ListNode {
     int val;
     ListNode *next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* save(ListNode *head){
-        ListNode* temp=head;
-        return head;
-    }
     ListNode* deleteDuplicates(ListNode* head) {
-        save(head);
-        if (head!=NULL)
-        {
-            return save(head);
+    if(head==nullptr||head->next==nullptr)
+    return head;
+    ListNode * curr=head;
+    while(curr!=nullptr&&curr->next!=nullptr){
+        if(curr->val==curr->next->val){
+            ListNode * temp=curr->next;
+            curr->next=curr->next->next;
+            delete temp;
         }
-        
-        ListNode *temp1;
-        ListNode *temp2;
-        temp1=head;
-        temp2=head;
-        while (temp1->val==temp2->val)
-        {
-            temp2=temp2->next;
+        else{
+            curr=curr->next;
         }
-        temp1->next=temp2;
-        deleteDuplicates(temp1); 
+    }
+    return head;
+
     }
 };
-int main()
-{
-    return 0;
-}
